be59bad65d2e153a48333a58e9e15995b4cf6112
unknown <calyston.14@gmail.com>
2020-09-10 09:11:06 +0100

    style: benefits header styled

---
+++ main.js
@@ -0,0 +1,13 @@
+//Smooth Scrolling
+
+function toLocations() {
+  document.querySelector("#locations").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function toBenefits() {
+  document.querySelector("#benefits").scrollIntoView({
+    behavior: "smooth"
+  });
+}
